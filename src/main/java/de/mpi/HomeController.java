b4f2ace5e52d724c097e0c753f7521ee96e34ff7
MuPiehl <matthias.piehl@hypoport.de>
2015-12-26 21:03:08 +0100

    iii

---
+++ src/main/java/de/mpi/HomeController.java
@@ -0,0 +1,18 @@
+package de.mpi;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by matthiaspiehl on 14.03.15.
+ */
+@Controller
+class HomeController {
+
+    @RequestMapping("/")
+//@ResponseBody
+    String index() {
+        return "index";
+    }
+}
---
+++ src/main/java/de/mpi/TestGitAdd.java
@@ -0,0 +1,7 @@
+package de.mpi;
+
+/**
+ * Created by matthiaspiehl on 26.12.15.
+ */
+public class TestGitAdd {
+}
